9264432256ef97643dfeac7a10799d747fee3e58
Petteri <petteri.koivuniemi@live.fi>
2020-11-09 14:21:35 +0200

    Refactor static content format for increased ease of use, add debug.log to .gitignore

--- cv-app/src/Components/Topic.js
+++ cv-app/src/Components/Topic.js
@@ -2,21 +2,25 @@ import App from './App';
 
 function Topic(props) {
     const content = App.GetContent(props.id);
-    const TopicContents = () => {
-        let len = content.subtitles?.length;
-        const elements = [];
-        if (len > 0) {
-            for (let i = 0; i < len; i++) {
-                elements.push(<p><strong>{content.subtitles[i]}</strong></p>);
-            }
-        }
-        else console.log(content.subtitles);
-        
-        return elements;
-     }
+    
     return (
         <div className="topic-main">
             <h2>{content?.title}</h2>
+
+            {
+                content?.parts?.map((topic) => {
+                    const elems = [
+                        <div className="header-row">
+                            <p><strong>{topic.header}</strong></p>
+                            <p><strong>{topic.date}</strong></p>
+                        </div>,
+                        <p>{topic.textcontent}</p>
+                    ];
+                    return elems;
+                 }
+                )
+            }
+
             <p>{content?.subtitles}</p>
             <p>{content?.textcontents}</p>
         </div>
