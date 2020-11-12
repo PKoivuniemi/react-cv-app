0fea666e490d0eb99a1b28b5ac424baaf3f68b4c
Petteri <petteri.koivuniemi@live.fi>
2020-11-12 16:24:11 +0200

    Update InputField component

--- cv-app/src/Components/InputField.js
+++ cv-app/src/Components/InputField.js
@@ -1,7 +1,13 @@
+import React, { useState } from 'react';
+
 function InputField(props) {
+    const [text, setText] = useState('');
     return (
         <div>
-
+            <input type="text"
+                value={state} name={name}
+                onChange={ (e) => setText(e.target.value) }
+            />
         </div>
     );
 }
