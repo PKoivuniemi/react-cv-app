16f0d61074f918e7e66b80277c2a5fa66149cb43
Petteri <petteri.koivuniemi@live.fi>
2020-11-12 18:49:38 +0200

    Add ContentEditor component

---
+++ cv-app/src/Components/ContentEditor.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+
+function ContentEditor(props) {
+
+    const editedHeader = useRef("");
+    const editedDate = useRef("");
+    const editedContent = useRef("");
+
+    function setCvData() {
+        props.setCvData(props.id, editedHeader.current.value,
+            editedDate.current.value,
+            editedContent.current.value);
+     }
+
+    return (
+        <div className="content-editor">
+            <form>
+                <div className="header-row">
+                    <input ref={editedHeader} name='header' defaultValue={props.header} />
+                    <input ref={editedDate} name="date" defaultValue={props.date} />
+                </div>
+                <textarea className="content-editor-textfield" ref={editedContent} name="textcontent">{props.textcontent}</textarea>,
+            <button onClick={e => { e.preventDefault(); setCvData() }}>Tallenna</button>
+            </form>
+        </div>
+    );
+}
+export default ContentEditor;
