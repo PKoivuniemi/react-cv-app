3befd6da9e3adf3664754edaf44e123d33d6c28e
Petteri <petteri.koivuniemi@live.fi>
2020-11-15 16:04:35 +0200

    Add comments to ContentEditor

--- cv-app/src/Components/ContentEditor.js
+++ cv-app/src/Components/ContentEditor.js
@@ -2,10 +2,12 @@ import React, { useRef } from 'react';
 
 function ContentEditor(props) {
 
+    //refs for different input fields
     const editedHeader = useRef("");
     const editedDate = useRef("");
     const editedContent = useRef("");
 
+    //Updates the displayed content for the given entry
     function setCvData() {
         props.setCvData(props.id, editedHeader.current.value,
             editedDate.current.value,
@@ -23,7 +25,7 @@ function ContentEditor(props) {
                     <input ref={editedHeader} name='header' defaultValue={props.header} />
                     <input ref={editedDate} name="date" defaultValue={props.date} />
                 </div>
-                <textarea className="content-editor-textfield" ref={editedContent} name="textcontent">{props.textcontent}</textarea>
+                <textarea className="content-editor-textfield" ref={editedContent} name="textcontent" defaultValue={props.textcontent}/>
                 <button onClick={e => { e.preventDefault(); removeCvData() }}>Poista</button>
             <button onClick={e => { e.preventDefault(); setCvData() }}>Tallenna</button>
             </form>
