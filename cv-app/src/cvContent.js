8503c3362ba3e9264f568c9a348bb1303abd2e4c
Petteri <petteri.koivuniemi@live.fi>
2020-11-15 14:03:43 +0200

    Add some default content to cv

---
+++ cv-app/src/cvContent.js
@@ -0,0 +1,94 @@
+const content =
+[
+  {
+    id: 0,
+    title: "Työkokemus",
+    parts: [
+      {
+        header: "Työharjoittelu",
+        date: "9/2019 – 12/2019",
+        textcontent: "Olin syksyllä 2019 kolme kuukautta työharjoittelussa Tamperelaisella Skydome Entertainmentilla. Työtehtäviini kuului Unityssä toteutettavan pelin käyttöliittymän ja siitä riippuvien toimintojen ohjelmoiminen."
+      },
+      {
+        header: "Vapaaehtoistyö",
+        date: "6/2016, 6/2017, 6/2018",
+        textcontent: "Vuosina 2016-2018 toimin vapaaehtoistyöntekijänä L2P-pelitapahtumassa. Työnkuvaani kuului tapahtumavieraiden neuvominen ja ohjaaminen sekä tapahtuman järjestelyissä auttaminen."
+      },
+      {
+        header: "Työharjoittelu",
+        date: "5/2017 – 7/2017",
+        textcontent: "Olin kesällä 2017 kaksi kuukautta työharjoittelussa Kangasalalaisessa Harjula Productionissa. Pääasiallinen työtehtäväni oli kehittää paikalliselle pelitapahtumalle responsiiviset verkkosivut. Tein myös 3D-mallinnusta tapahtumassa esiteltyyn VR-demoon."
+      },
+      {
+        header: "Muuta: asepalvelus erikoistehtävissä",
+        date: "7/2014 – 3/2015",
+        textcontent: "Tein asepalveluksen aikana kuusi kuukautta 3D-mallintamista Blenderilla Parolan Panssariprikaatin simulaattorihallilla. Mallinsin Puolustusvoimien kalustoa VBS-taistelusimulaattoriin miehistön virtuaaliharjoituksia varten. Lisäksi tehtäviini kuului teknisenä tukena toimiminen harjoitusten aikana."  
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: "Koulutus",
+    parts: [
+      {
+        header: "Tradenomi",
+        date: "2015 - 2019",
+        textcontent: "Tampereen ammattikorkeakoulu, tietojenkäsittelyn koulutusohjelma"
+      },
+      {
+        header: "Lukio",
+        date: "2014",
+        textcontent: "Kangasalan Lukio"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Osaaminen",
+    parts: [
+      {
+        header: "Web-tekniikat",
+        date: "",
+        textcontent: "Html ja CSS ovat tulleet opinnoissa tutuiksi. Lisäksi opin JavaScriptin perusteet tradenomin tutkintoa suorittaessa, tuoreempaa harjoitusta moderneihin frameworkeihin kuten Reactiin on alkanut kertyä syksyn 2020 edetessä. Backend-puolella olen käyttänyt php:n ja mySQL:n yhdistelmää."
+      },
+      {
+        header: "Versionhallinta",
+        date: "",
+        textcontent: "Olen käyttänyt git-versionhallintaa opintojen ja harrastusten puitteissa noin kaksi vuotta. Pääsääntöisesti olen käyttänyt Sourcetreetä käyttöliittymänä gitin kanssa. Otan syksyn 2020 edetessä osaa avoimen ammattikoulun fullstack-kurssiin, mistä olen oppinut gitin terminaalikomentoja ja alkanut hahmottaa isojen tiimien kannalta oikeaoppista gitin käyttöä."
+      },
+      {
+        header: "Unity ja C#",
+        date: "",
+        textcontent: "Ensikosketukseni Unityyn sain lukiossa järjestetyllä pelinkehityskurssilla vuoden 2013 tienoilla. Sittemmin saamieni ohjelmointioppien myötä olen alkanut harrastella aktiivisemmin pienten peliprojektien parissa ja Unityssä pääasiallisena ohjelmointikielenä käytetty C# on tullut tutuksi."
+      },
+      {
+        header: "Muu ohjelmointi",
+        date: "",
+        textcontent: "Edellä mainittujen kielten lisäksi tutkintooni sisältyi kursseja useammasta ohjelmointikielestä, jotka jäivät opintojen edetessä vähemmälle käytölle. Olen käynyt peruskurssit mm. Javasta, C:stä ja C++:sta."
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Kielitaito",
+    parts: [
+      {
+        header: "Suomi",
+        date: "Äidinkieli",
+        textcontent: ""
+      },
+      {
+        header: "Englanti",
+        date: "Erinomainen",
+        textcontent: ""
+      },
+      {
+        header: "Ruotsi",
+        date: "Tyydyttävä",
+        textcontent: ""
+      }
+    ]
+  }
+];
+
+export default content;
